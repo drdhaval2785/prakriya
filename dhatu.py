6d12aae2f46d5a4f8d8a8499b31a1416fa6612d0
drdhaval2785 <drdhaval2785#gmail.com>
2017-12-15 13:26:13 +0530

    readable prakriya

--- dhatu.py
+++ dhatu.py
@@ -26,31 +26,30 @@ def get_full_data(verbform):
 
 @app.route('/api/v0.0.1/<string:verbform>/prakriya', methods=['GET'])
 def get_prakriya(verbform):
-    """Return the specific sought for information of a given verb form.
-
-    """
+    """Return the specific sought for information of a given verb form."""
     fileofinterest = 'data/json/' + verbform + '.json'
     if not os.path.exists('data/sutrainfo.json'):
         return jsonify({'error': 'file data/sutrainfo.json missing. You can obtain it from https://github.com/drdhaval2785/SanskritVerb/blob/master/Data/sutrainfo.json'})
     elif os.path.exists(fileofinterest):
         with open(fileofinterest, 'r') as fin:
             verbdata = json.load(fin)
-            data = verbdata['derivation']
             result = []
             with open('data/sutrainfo.json', 'r') as sutrafile:
                 sutrainfo = json.load(sutrafile)
-                for member in data:
-                    result.append((member['sutra_num'], sutrainfo[member['sutra_num']], ','.join(member['text'])))
-                return jsonify(result)
+            data = verbdata
+            for datum in data:
+                subresult = []
+                for member in datum['derivation']:
+                    subresult.append(sutrainfo[member['sutra_num']] + ' (' + member['sutra_num'] + ') -> ' + ','.join(member['text']))
+                result.append(subresult)
+            return jsonify(result)
     else:
         return jsonify({'error': 'The verb form is not in our database. If you feel it deserves to be included, kindly notify us on https://github.com/drdhaval2785/sktderivation/issues.'})
 
 
 @app.route('/api/v0.0.1/<string:verbform>/<string:argument>', methods=['GET'])
 def get_meaning(verbform, argument):
-    """Return the specific sought for information of a given verb form.
-
-    """
+    """Return the specific sought for information of a given verb form."""
     with open('data/json/' + verbform + '.json', 'r') as fin:
         verbdata = json.load(fin)
         return jsonify(verbdata[argument])
