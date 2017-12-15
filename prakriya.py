dc39a18f9e785e4cee92960d6ef6efd5b0987bc1
drdhaval2785 <drdhaval2785#gmail.com>
2017-12-15 20:07:54 +0530

    CLI working

--- prakriya.py
+++ prakriya.py
@@ -18,18 +18,16 @@ def get_full_data(verbform):
     The files are created by the script at SanskritVerb repository.
     See https://github.com/drdhaval2785/SanskritVerb/blob/master/jsongenerator.sh for details.
     """
-    uris = giveuris(verbform)
     fileofinterest = 'data/json/' + verbform + '.json'
     if os.path.exists(fileofinterest):
         with open(fileofinterest, 'r') as fin:
-            return json.dumps({'uris': uris, 'result': json.load(fin)})
+            return json.dumps(json.load(fin))
     else:
-        return json.dumps({'uris': uris, 'error': 'The verb form is not in our database. If you feel it deserves to be included, kindly notify us on https://github.com/drdhaval2785/sktderivation/issues.'})
+        return json.dumps({'error': 'The verb form is not in our database. If you feel it deserves to be included, kindly notify us on https://github.com/drdhaval2785/sktderivation/issues.'})
 
 
 def get_specific_info(verbform, argument):
     """Return the specific sought for information of a given verb form."""
-    uris = giveuris(verbform)
     with open('data/json/' + verbform + '.json', 'r') as fin:
         verbdata = json.load(fin)
         tmp = []
@@ -40,33 +38,51 @@ def get_specific_info(verbform, argument):
         for member in tmp:
             if member not in result:
                 result.append(member)
-        return json.dumps({'uris': uris, 'result': result})
+        return json.dumps(result)
 
 
-def giveuris(verbform='<verbform>', webserver='127.0.0.1:5000', version='v0.0.1'):
-    """Give the URIs list for RESTful service."""
-    uris = {'prakriya_human_readable': webserver + '/api/' + version + '/' + verbform + '/prakriya',
-            'prakriya_machine_readable': webserver + '/api/' + version + '/' + verbform + '/prakriya/machine',
-            'verb_devanagari': webserver + '/api/' + version + '/' + verbform + '/verb',
-            'verb_meaning': webserver + '/api/' + version + '/' + verbform + '/meaning',
-            'verb_number': webserver + '/api/' + version + '/' + verbform + '/number',
-            'verb_gana': webserver + '/api/' + version + '/' + verbform + '/gana',
-            'madhaviyadhatuvritti_link': webserver + '/api/' + version + '/' + verbform + '/madhaviya',
-            'kshiratarangini_link': webserver + '/api/' + version + '/' + verbform + '/kshiratarangini',
-            'dhatupradipa_link': webserver + '/api/' + version + '/' + verbform + '/dhatupradipa',
-            'UoHyd_link': webserver + '/api/' + version + '/' + verbform + '/uohyd',
-            'JNU_link': webserver + '/api/' + version + '/' + verbform + '/jnu',
-            'verb_slp': webserver + '/api/' + version + '/' + verbform + '/verbslp',
-            'lakAra_or_tense': webserver + '/api/' + version + '/' + verbform + '/lakara',
-            'upasarga': webserver + '/api/' + version + '/' + verbform + '/upasarga',
-            'padadecider_id': webserver + '/api/' + version + '/' + verbform + '/padadecider_id',
-            'padadecider_sutra': webserver + '/api/' + version + '/' + verbform + '/padadecider_sutra',
-            'it_sutra': webserver + '/api/' + version + '/' + verbform + '/it_sutra',
-            'it_id': webserver + '/api/' + version + '/' + verbform + '/it_id',
-            'it_status': webserver + '/api/' + version + '/' + verbform + '/it_status',
-            'all_details': webserver + '/api/' + version + '/' + verbform,
-            }
-    return uris
+def get_prakriya(verbform):
+    """Return the specific sought for information of a given verb form."""
+    fileofinterest = 'data/json/' + verbform + '.json'
+    if not os.path.exists('data/sutrainfo.json'):
+        return json.dumps({'error': 'file data/sutrainfo.json missing. You can obtain it from https://github.com/drdhaval2785/SanskritVerb/blob/master/Data/sutrainfo.json'})
+    elif os.path.exists(fileofinterest):
+        with open(fileofinterest, 'r') as fin:
+            verbdata = json.load(fin)
+            result = []
+            with open('data/sutrainfo.json', 'r') as sutrafile:
+                sutrainfo = json.load(sutrafile)
+            data = verbdata
+            for datum in data:
+                subresult = []
+                for member in datum['derivation']:
+                    subresult.append(sutrainfo[member['sutra_num']] + ' (' + member['sutra_num'] + ') -> ' + ','.join(member['text']))
+                result.append(subresult)
+            return json.dumps({'message': 'for machine friendly output, use /api/v0.0.1/verbform/prakriya/jsonified', 'result': result}, indent=4)
+    else:
+        return json.dumps({'error': 'The verb form is not in our database. If you feel it deserves to be included, kindly notify us on https://github.com/drdhaval2785/sktderivation/issues.'})
+
+
+def get_prakriya_jsonified(verbform):
+    """Return the specific sought for information of a given verb form."""
+    fileofinterest = 'data/json/' + verbform + '.json'
+    if not os.path.exists('data/sutrainfo.json'):
+        return json.dumps({'error': 'file data/sutrainfo.json missing. You can obtain it from https://github.com/drdhaval2785/SanskritVerb/blob/master/Data/sutrainfo.json'})
+    elif os.path.exists(fileofinterest):
+        with open(fileofinterest, 'r') as fin:
+            verbdata = json.load(fin)
+            result = []
+            with open('data/sutrainfo.json', 'r') as sutrafile:
+                sutrainfo = json.load(sutrafile)
+            data = verbdata
+            for datum in data:
+                subresult = []
+                for member in datum['derivation']:
+                    subresult.append((sutrainfo[member['sutra_num']], member['sutra_num'], ','.join(member['text'])))
+                result.append(subresult)
+            return json.dumps({'message': 'for human readable output, use `python prakriya.py verbform prakriya`', 'result': result}, indent=4)
+    else:
+        return json.dumps({'error': 'The verb form is not in our database. If you feel it deserves to be included, kindly notify us on https://github.com/drdhaval2785/sktderivation/issues.'})
 
 
 def maparguments(argument):
@@ -93,18 +109,28 @@ def maparguments(argument):
 
 
 if __name__ == '__main__':
-    # uris = giveuris()
     print(timestamp())
     syslen = len(sys.argv)
+    if syslen < 2 or syslen > 4:
+        print json.dumps({'error': 'Kindly use the following syntax. `python prakriya.py verbform [argument] [readability]`.'})
+        exit(0)
+    elif syslen == 4 and not sys.argv[3] == 'machine':
+        print json.dumps({'error': 'The third argument can only be `machine`.'})
+        exit(0)
+
     if syslen >= 2:
         verbform = sys.argv[1]
     if syslen >= 3:
         argument = sys.argv[2]
-    if syslen >= 4:
+    if syslen == 4:
         readability = sys.argv[3]
-    if syslen < 2:
-        print json.dumps({'error': 'Kindly use the following syntax. `python prakriya.py verbform [argument] [readability]`.'})
-        exit(0)
-    # print(get_full_data(verbform))
-    print(get_specific_info(verbform, argument))
+
+    if syslen == 4 and readability == 'machine':
+        print(get_prakriya_jsonified(verbform))
+    elif syslen == 3 and argument == 'prakriya':
+        print(get_prakriya(verbform))
+    elif syslen == 3:
+        print(get_specific_info(verbform, argument))
+    elif syslen == 2:
+        print(get_full_data(verbform))
     print(timestamp())
